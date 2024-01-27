8f2aa7e18439da5f538c2fcb08959e9a96b04963
serifagir <serifagir64@gmail.com>
2024-01-27 17:00:32 +0300

    Session sliders

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flux/components/main_page_components/session_control_buttons.dart';
-
 import 'package:flux/components/main_page_components/session_timer.dart';
+import 'package:flux/provider/flux_configure_provider.dart';
+import 'package:flux/provider/time_provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -26,24 +28,7 @@ class MainPage extends StatelessWidget {
               left: size.width * 0.13,
               child: IconButton(
                   onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) => const AlertDialog(
-                            title: Text("Configure Flux"),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                FluxDurationForm(),
-                                SizedBox(height: 10),
-                                BreakDurationForm(),
-                                SizedBox(height: 10),
-                                LongBreakDurationForm(),
-                                SizedBox(height: 10),
-                                SessionCountForm(),
-                                SizedBox(height: 10),
-                                SamuraiModeForm(),
-                              ],
-                            )));
+                    fluxConfigureDialog(context);
                   },
                   icon: const Icon(
                     CupertinoIcons.waveform,
@@ -76,134 +61,133 @@ class MainPage extends StatelessWidget {
       )),
     );
   }
+
+  Future<dynamic> fluxConfigureDialog(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fluxConfigureProvider =
+        Provider.of<FluxConfigureProvider>(context, listen: false);
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            title: const Text("Configure Flux"),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DurationWidget(
+                  title: 'flux duration',
+                  sliderValue: FluxConfigureProvider.fluxDurationValue,
+                  max: 60,
+                  min: 15,
+                  updateValue: (newValue) {
+                    fluxConfigureProvider.updateFluxDurationValue(newValue);
+                  },
+                  minText: 'min',
+                ),
+                DurationWidget(
+                  title: 'break duration',
+                  sliderValue: FluxConfigureProvider.breakDurationValue,
+                  max: 20,
+                  min: 5,
+                  updateValue: (newValue) {
+                    fluxConfigureProvider.updateBreakDurationValue(newValue);
+                  },
+                  minText: 'min',
+                ),
+                DurationWidget(
+                  title: 'long break duration',
+                  sliderValue: FluxConfigureProvider.longBreakDurationValue,
+                  max: 30,
+                  min: 10,
+                  updateValue: (newValue) {
+                    fluxConfigureProvider
+                        .updateLongBreakDurationValue(newValue);
+                  },
+                  minText: 'min',
+                ),
+                DurationWidget(
+                  title: 'session count',
+                  sliderValue: FluxConfigureProvider.sessionCountValue,
+                  max: 4,
+                  min: 2,
+                  updateValue: (newValue) {
+                    fluxConfigureProvider.updateSessionCountValue(newValue);
+                  },
+                  minText: '',
+                ),
+                SamuraiModeForm()
+              ],
+            )));
+  }
 }
 
-class FluxDurationForm extends StatelessWidget {
-  const FluxDurationForm({
+class DurationWidget extends StatelessWidget {
+  DurationWidget({
     super.key,
+    required this.title,
+    required this.sliderValue,
+    required this.max,
+    required this.min,
+    required this.updateValue,
+    required this.minText,
   });
+  final String title;
+  final double max;
+  final double min;
+  int sliderValue;
+  String minText;
+  void Function(int newValue) updateValue;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      isExpanded: false,
-      icon: SizedBox.shrink(),
-      onChanged: (value) {},
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          labelText: "Flux Duration"),
-      items: <String>['10', '15', '25', '30', '45', '50', '60', 'Custom']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
+    final timerProvider = Provider.of<TimerProvider>(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-        );
-      }).toList(),
+            Slider(
+              label: "$sliderValue",
+              max: max,
+              min: min,
+              value: sliderValue.toDouble(),
+              onChanged: (value) {
+                sliderValue = value.toInt();
+                updateValue(sliderValue);
+                timerProvider.resetTimer();
+              },
+            ),
+          ],
+        ),
+        TextWithPadding(text: "$sliderValue $minText"),
+      ],
     );
   }
 }
 
-class BreakDurationForm extends StatelessWidget {
-  const BreakDurationForm({
+class TextWithPadding extends StatelessWidget {
+  const TextWithPadding({
+    required this.text,
     super.key,
   });
-
+  final String text;
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      icon: SizedBox.shrink(),
-      onChanged: (value) {},
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          labelText: "Break Duration"),
-      items: <String>['1', '5', '10', '15', 'Custom']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: GoogleFonts.poppins(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class LongBreakDurationForm extends StatelessWidget {
-  const LongBreakDurationForm({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      icon: SizedBox.shrink(),
-      onChanged: (value) {},
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          labelText: "Long Break Duration"),
-      items: <String>['20', '30', '40', 'Custom']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: GoogleFonts.poppins(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class SessionCountForm extends StatelessWidget {
-  const SessionCountForm({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      icon: SizedBox.shrink(),
-      onChanged: (value) {},
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          labelText: "Session Count"),
-      items: <String>['2', '3', '4', 'Custom']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: GoogleFonts.poppins(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        );
-      }).toList(),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Text(text, style: Theme.of(context).textTheme.titleMedium),
     );
   }
 }
