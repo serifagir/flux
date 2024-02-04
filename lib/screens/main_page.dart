13d8945bee9805685cefc839cb38bf7c398809e5
serifagir <serifagir64@gmail.com>
2024-02-04 22:29:33 +0300

    new fluxconfigure

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -2,11 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flux/components/main_page_components/session_control_buttons.dart';
 import 'package:flux/components/main_page_components/session_timer.dart';
+import 'package:flux/models/flux.dart';
 import 'package:flux/provider/flux_configure_provider.dart';
 import 'package:flux/provider/samurai_mode_provider.dart';
 import 'package:flux/provider/time_provider.dart';
 import 'package:flux/screens/settings_page.dart';
-import 'package:flux/screens/stats_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -17,170 +17,115 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final timerProvider = Provider.of<TimerProvider>(context);
+    final fluxConfigureProvider = Provider.of<FluxConfigureProvider>(context);
     return Scaffold(
       body: Center(
           child: Stack(
+        alignment: AlignmentDirectional.center,
         children: [
-          Positioned(
-              bottom: size.width * 0.1,
-              left: size.width * 0.02,
-              child: timerProvider.isRunning
-                  ? const SizedBox()
-                  : IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SettingsPage()));
-                      },
-                      icon: const Icon(CupertinoIcons.gear_alt, size: 30))),
-          Positioned(
-              bottom: size.width * 0.1,
-              left: size.width * 0.13,
-              child: timerProvider.isRunning
-                  ? const SizedBox()
-                  : IconButton(
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) => FluxConfigureDialog(
-                                fluxConfigureProvider:
-                                    Provider.of<FluxConfigureProvider>(context,
-                                        listen: false)));
-                      },
-                      icon: const Icon(
-                        CupertinoIcons.waveform,
-                        size: 30,
-                      ))),
-          Positioned(
-              bottom: size.width * 0.1,
-              right: size.width * 0.02,
-              child: IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const StatsPage()));
-                  },
-                  icon: const Icon(CupertinoIcons.chart_bar_square, size: 30))),
-          // Positioned(
-          //     bottom: size.width * 0.1,
-          //     right: size.width * 0.13,
-          //     child: IconButton(
-          //         onPressed: () {
-          //           showDialog(
-          //               context: context,
-          //               builder: (context) => const CustomizePage());
-          //         },
-          //         icon: const Icon(
-          //           CupertinoIcons.paintbrush,
-          //           size: 30,
-          //         ))),
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SessionTimer(),
-              SessionControlButtons(),
-            ],
+          SettingsPageNavigateButton(size: size, timerProvider: timerProvider),
+          FluxConfigureDialogButton(
+            size: size,
+            timerProvider: timerProvider,
+            fluxConfigureProvider: fluxConfigureProvider,
           ),
+          StatsPageNavigateButton(size: size),
+          CustomizePageNavigateButton(size: size),
+          const SessionTimer(),
+          const SessionControlButtons()
         ],
       )),
     );
   }
 }
 
-class FluxConfigureDialog extends StatelessWidget {
-  const FluxConfigureDialog({
-    super.key,
-    required this.fluxConfigureProvider,
-  });
+class FluxConfigureDialogButton extends StatelessWidget {
+  const FluxConfigureDialogButton(
+      {super.key,
+      required this.size,
+      required this.timerProvider,
+      required this.fluxConfigureProvider});
 
+  final Size size;
+  final TimerProvider timerProvider;
   final FluxConfigureProvider fluxConfigureProvider;
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return AlertDialog(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(
-          "Configure Flux",
-          style: GoogleFonts.poppins(
-            color: Theme.of(context).colorScheme.primary,
-            fontSize: 25,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            DurationWidget(
-              title: 'flux duration',
-              sliderValue: FluxConfigureProvider.fluxDurationValue,
-              max: 60,
-              min: 1,
-              updateValue: (newValue) {
-                fluxConfigureProvider.updateFluxDurationValue(newValue);
-              },
-              minText: 'min',
-            ),
-            DurationWidget(
-              title: 'break duration',
-              sliderValue: FluxConfigureProvider.breakDurationValue,
-              max: 20,
-              min: 1,
-              updateValue: (newValue) {
-                fluxConfigureProvider.updateBreakDurationValue(newValue);
-              },
-              minText: 'min',
-            ),
-            DurationWidget(
-              title: 'long break duration',
-              sliderValue: FluxConfigureProvider.longBreakDurationValue,
-              max: 30,
-              min: 1,
-              updateValue: (newValue) {
-                fluxConfigureProvider.updateLongBreakDurationValue(newValue);
-              },
-              minText: 'min',
-            ),
-            DurationWidget(
-              title: 'session count',
-              sliderValue: FluxConfigureProvider.sessionCountValue,
-              max: 4,
-              min: 2,
-              updateValue: (newValue) {
-                fluxConfigureProvider.updateSessionCountValue(newValue);
-              },
-              minText: '',
-            ),
-            const SamuraiModeForm(),
-            SizedBox(height: size.height * 0.02),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Text("Want longer durations? Tap to upgrade to premium!",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                          )),
-                ),
-              ),
-            ),
-          ],
-        ));
+    return Positioned(
+        bottom: size.width * 0.1,
+        left: size.width * 0.13,
+        child: timerProvider.isRunning
+            ? const SizedBox()
+            : IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => FluxConfigureDialog());
+                },
+                icon: const Icon(
+                  CupertinoIcons.waveform,
+                  size: 30,
+                )));
   }
 }
 
+class FluxConfigureDialog extends StatelessWidget {
+  const FluxConfigureDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final fluxConfigureProvider = Provider.of<FluxConfigureProvider>(context);
+    return Column(
+      children: [
+        DurationWidget(
+          title: 'Study Duration',
+          sliderValue: FluxConfigureProvider.fluxDurationValue,
+          max: 60,
+          min: 5,
+          updateValue: (newValue) {
+            fluxConfigureProvider.updateFluxDurationValue(newValue);
+          },
+          minText: 'min',
+        ),
+        DurationWidget(
+          title: 'Short break duration',
+          sliderValue: FluxConfigureProvider.breakDurationValue,
+          max: 30,
+          min: 1,
+          updateValue: (newValue) {
+            fluxConfigureProvider.updateBreakDurationValue(newValue);
+          },
+          minText: 'min',
+        ),
+        DurationWidget(
+          title: 'Long break duration',
+          sliderValue: FluxConfigureProvider.longBreakDurationValue,
+          max: 45,
+          min: 1,
+          updateValue: (newValue) {
+            fluxConfigureProvider.updateLongBreakDurationValue(newValue);
+          },
+          minText: 'min',
+        ),
+        DurationWidget(
+          title: 'Sessions',
+          sliderValue: FluxConfigureProvider.sessionCountValue,
+          max: 15,
+          min: 2,
+          updateValue: (newValue) {
+            fluxConfigureProvider.updateSessionCountValue(newValue);
+          },
+          minText: '',
+        ),
+      ],
+    );
+  }
+}
+
+// ignore: must_be_immutable
 class DurationWidget extends StatelessWidget {
   DurationWidget({
     super.key,
@@ -203,50 +148,50 @@ class DurationWidget extends StatelessWidget {
     final timerProvider = Provider.of<TimerProvider>(context);
     return Column(
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  title,
-                  softWrap: true,
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                Text(
-                  " $sliderValue $minText",
-                  softWrap: true,
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
-            Slider(
-              label: "$sliderValue",
-              divisions: 6,
-              max: max,
-              min: min,
-              value: sliderValue.toDouble(),
-              onChanged: (value) {
-                sliderValue = value.toInt();
-                updateValue(sliderValue);
-                timerProvider.resetTimer();
-              },
-            ),
-          ],
+        SizedBox(
+          height: 70,
+          child: Stack(
+            children: [
+              TextWithPadding(text: title),
+              Slider(
+                label: "$sliderValue",
+                max: max,
+                min: min,
+                value: sliderValue.toDouble(),
+                onChanged: (value) {
+                  sliderValue = value.toInt();
+                  updateValue(sliderValue);
+                  timerProvider.resetTimer();
+                },
+              ),
+              Align(
+                  alignment: AlignmentDirectional.bottomStart,
+                  child: Text('${min.toInt()} $minText')),
+              Align(
+                  alignment: AlignmentDirectional.bottomEnd,
+                  child: Text('${max.toInt()} $minText')),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 15,
         ),
       ],
+    );
+  }
+}
+
+class TextWithPadding extends StatelessWidget {
+  const TextWithPadding({
+    required this.text,
+    super.key,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Text(text, style: Theme.of(context).textTheme.titleMedium),
     );
   }
 }
@@ -308,5 +253,81 @@ class SamuraiModeForm extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class SettingsPageNavigateButton extends StatelessWidget {
+  const SettingsPageNavigateButton({
+    super.key,
+    required this.size,
+    required this.timerProvider,
+  });
+
+  final Size size;
+  final TimerProvider timerProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: size.width * 0.1,
+        left: size.width * 0.02,
+        child: timerProvider.isRunning
+            ? const SizedBox()
+            : IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SettingsPage()));
+                },
+                icon: const Icon(CupertinoIcons.gear_alt, size: 30)));
+  }
+}
+
+class StatsPageNavigateButton extends StatelessWidget {
+  const StatsPageNavigateButton({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: size.width * 0.1,
+        right: size.width * 0.02,
+        child: IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Text("null")));
+            },
+            icon: const Icon(CupertinoIcons.chart_bar_square, size: 30)));
+  }
+}
+
+class CustomizePageNavigateButton extends StatelessWidget {
+  const CustomizePageNavigateButton({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: size.width * 0.1,
+        right: size.width * 0.13,
+        child: IconButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) => const Text("customize page"));
+            },
+            icon: const Icon(
+              CupertinoIcons.paintbrush,
+              size: 30,
+            )));
   }
 }
