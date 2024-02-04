0b27548bb9ed2967cee98e3f49ad3cdf33737e1f
serifagir <serifagir64@gmail.com>
2024-02-04 22:22:48 +0300

    Revert "some ui improvements"
    This reverts commit 1c5628d4e7406c84114b847177eadb36bd243344.

--- lib/provider/stats_provider.dart
+++ lib/provider/stats_provider.dart
@@ -3,42 +3,8 @@ import 'package:flux/models/flux.dart';
 
 class StatsProvider extends ChangeNotifier {
   int _totalTime = 0;
-  int _totalSessions = 0;
 
-  final List<Flux> _completedFluxes = [
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now(),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 1)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 2)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 2)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 3)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 3)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 3)),
-    ),
-    Flux(
-      fluxDuration: 100,
-      date: DateTime.now().subtract(const Duration(days: 4)),
-    ),
-  ];
+  List<Flux> _completedFluxes = [];
 
   int get totalTime => _totalTime;
 
@@ -50,20 +16,6 @@ class StatsProvider extends ChangeNotifier {
 
   void addTotalTime(int time) {
     _totalTime += time;
-    notifyListeners();
-  }
-
-  void dailyDataHandler(DateTime date) {
-    _totalTime = 0;
-    _totalSessions = 0;
-    _completedFluxes.forEach((flux) {
-      _totalSessions++;
-      if (flux.date.day == date.day &&
-          flux.date.month == date.month &&
-          flux.date.year == date.year) {
-        _totalTime += flux.fluxDuration;
-      }
-    });
     notifyListeners();
   }
 }
